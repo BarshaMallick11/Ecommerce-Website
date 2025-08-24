f99ebf4437daf7d1796b6a3fd68b33076a1183b3
Barsha11 <barshamallick57@gmail.com>
2025-08-25 01:26:04 +0530

    configure CORS properly

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
